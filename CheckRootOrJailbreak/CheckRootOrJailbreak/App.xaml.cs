569e7cf5e9671c7873acb9a830bf37081705adab
Marthin Silalahi <marthin.silalahi@anacle.com>
2020-10-07 09:35:23 +0800

    INITIALIZE PROJECT & GIT IGNORE

---
+++ CheckRootOrJailbreak/CheckRootOrJailbreak/App.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Plugin.BreachDetector;
+using Xamarin.Forms;
+
+namespace CheckRootOrJailbreak
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        private void CheckRootOrJailbreak()
+        {
+            bool isRootOrJailbreak = CrossBreachDetector.Current.IsRooted().GetValueOrDefault();
+            if (!isRootOrJailbreak)
+            {
+                Device.BeginInvokeOnMainThread(async () =>
+                {
+                    await App.Current.MainPage.DisplayAlert("", "Not supported on Rooted Devices", "Ok");
+
+                    // Kill process
+                    System.Diagnostics.Process.GetCurrentProcess().Kill();
+                });
+            }
+        }
+
+        protected override void OnStart()
+        {
+            CheckRootOrJailbreak();
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
